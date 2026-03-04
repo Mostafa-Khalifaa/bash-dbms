9b7ac78fe9accad5418c30922411f7fd6f9cf522
Mostafa-Khalifaa <mostafa.khalifa.2000.mk@gmail.com>
2026-03-04 15:19:35 +0200

    Project setup and script entry point done

---
+++ dbms.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+main_menu() {
+    echo "================================"
+    echo "    DATABASE MANAGEMENT SYSTEM  "
+    echo "================================"
+    echo "1) Create Database"
+    echo "2) List Databases"
+    echo "3) Connect to Database"
+    echo "4) Drop Database"
+    echo "5) Exit"
+    echo "================================"
+}
+
+main_menu
+
